51c9485f15118471f4c76bba2d5d06fcbdd25720
DilmurodAlimurodov <87900187+DilmurodAlimurodov@users.noreply.github.com>
2023-01-24 17:51:18 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -120,10 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 import os
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
